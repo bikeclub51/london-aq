e18ebd8e69d786c57464e77aa688d2907d841b9e
Alex Herrera <alexherr@mit.edu>
2022-02-15 01:12:49 -0500

    updated plots function

--- code/gp-models/sklearn_gpr.py
+++ code/gp-models/sklearn_gpr.py
@@ -13,6 +13,7 @@ import pickle
 import time
 import matplotlib.pyplot as plt
 import os
+import random
 
 def load_data(pollutant, data_path="data-collection/", timestep=None, subset=None, season=None, dayOfWeek=None, timeOfDay=None):
     """
@@ -28,18 +29,16 @@ def load_data(pollutant, data_path="data-collection/", timestep=None, subset=Non
     :param dayOfWeek: if provided, {"weekday", "weekend"}
     :param timeOfDay: if provided, {"day", "night"}
     """
-    df = pd.read_csv(f"{data_path}{pollutant}.csv", parse_dates=["Timestamp"]).set_index("Timestamp")
-    print(df)
-    print(type(df.index))
+    df = pd.read_csv(f"{data_path}{pollutant}.csv", parse_dates=["date"]).set_index("date")
 
     # daily, monthly, yearly
     if timestep in {"D", "M", "Y"}:
         index_format = {"D": "%Y-%m-%d", "M": "%Y-%m", "Y": "%Y"}
-        df = df.groupby(by=["SiteCode"]).resample(timestep).mean().dropna().reset_index()
+        df = df.groupby(by=["code"]).resample(timestep).mean().dropna().reset_index()
         # df["date"] = df["date"].apply(lambda x: x.strftime(index_format[timestep]))
 
     if subset:
-        df = df.loc[df["Timestamp"] > subset]
+        df = df.loc[df["date"] > subset]
 
     # seasonal buckets
     if season:
@@ -48,7 +47,7 @@ def load_data(pollutant, data_path="data-collection/", timestep=None, subset=Non
                 else [i for i in range(6, 9)] if season == "summer" \
                     else [i for i in range(9, 12)] if season == "autumn" else [i for i in range(12)]
 
-        df["Month"] = pd.DatetimeIndex(df['Timestamp']).month
+        df["Month"] = pd.DatetimeIndex(df['date']).month
         # print(df)
 
         # define condition: month must be within seasonal month range
@@ -57,9 +56,9 @@ def load_data(pollutant, data_path="data-collection/", timestep=None, subset=Non
             condition = ((df.Month >= months[0]) | (df.Month <= months[-1]))
         df = df.loc[condition]
         
-        print(df)
-        print("Season: ", season, " , ", months)
-        print("Season Months: ", df.Month.unique())
+        # print(df)
+        # print("Season: ", season, " , ", months)
+        # print("Season Months: ", df.Month.unique())
 
         # drop created month column
         df = df.drop(["Month"], axis=1)
@@ -69,7 +68,7 @@ def load_data(pollutant, data_path="data-collection/", timestep=None, subset=Non
     if dayOfWeek:
         days = [i for i in range(5)] if dayOfWeek == "weekday" else [i for i in range(5,7)]
 
-        df["DayOfWeek"] = pd.DatetimeIndex(df['Timestamp']).dayofweek
+        df["DayOfWeek"] = pd.DatetimeIndex(df['date']).dayofweek
 
         condition = ((df.DayOfWeek >= days[0]) & (df.DayOfWeek <= days[-1]))
         df = df.loc[condition]
@@ -84,7 +83,7 @@ def load_data(pollutant, data_path="data-collection/", timestep=None, subset=Non
     if timeOfDay:
         hours = [i for i in range(7, 18)] if timeOfDay == "day" else [i%24 for i in range(18, 31)]
 
-        df["Hour"] = pd.DatetimeIndex(df['Timestamp']).hour
+        df["Hour"] = pd.DatetimeIndex(df['date']).hour
 
         condition = ((df.Hour >= hours[0]) & (df.Hour <= hours[-1]))
         if timeOfDay == "night":
@@ -99,19 +98,27 @@ def load_data(pollutant, data_path="data-collection/", timestep=None, subset=Non
 
     return df
 
-def transform_data(df, pollutant):
+def transform_data(df, pollutant, timestep):
     N_MONTHS = 12
     N_DAYS = 30
     N_HOURS = 24
-    start_year = df["Timestamp"].min().year
-    df['t'] = df.apply(lambda row: (row.Timestamp.year-start_year)*N_MONTHS*N_DAYS*N_HOURS + (row.Timestamp.month%N_MONTHS)*N_DAYS*N_HOURS \
-        + (row.Timestamp.day%N_DAYS)*N_HOURS + (row.Timestamp.hour%N_HOURS), axis=1)
+    start_year = df["date"].min().year
+    if timestep == "H":
+        df['t'] = df.apply(lambda row: (row.date.year-start_year)*N_MONTHS*N_DAYS*N_HOURS + (row.date.month%N_MONTHS)*N_DAYS*N_HOURS \
+            + (row.date.day%N_DAYS)*N_HOURS + (row.date.hour%N_HOURS), axis=1)
+    elif timestep == "D":
+        df['t'] = df.apply(lambda row: (row.date.year-start_year)*N_MONTHS*N_DAYS + (row.date.month%N_MONTHS)*N_DAYS \
+            + (row.date.day%N_DAYS), axis=1)
+    elif timestep == "M":
+        df['t'] = df.apply(lambda row: (row.date.year-start_year)*N_MONTHS + (row.date.month%N_MONTHS), axis=1)
+    elif timestep == "Y":
+        df['t'] = df.apply(lambda row: row.date.year-start_year, axis=1)
     df = df.sort_values("t")
 
     lat_scaler = StandardScaler()
-    df["norm_lat"] = lat_scaler.fit_transform(df[["Latitude"]].values)
+    df["norm_lat"] = lat_scaler.fit_transform(df[["latitude"]].values)
     lon_scaler = StandardScaler()
-    df["norm_lon"] = lon_scaler.fit_transform(df[["Longitude"]].values)
+    df["norm_lon"] = lon_scaler.fit_transform(df[["longitude"]].values)
     pol_scaler = StandardScaler()
     df[f"norm_{pollutant}"] = pol_scaler.fit_transform(df[[pollutant]].values)
 
@@ -122,10 +129,30 @@ def get_X_Y_split(df, pollutant):
     Y = df[[f"norm_{pollutant}"]].values
     N, M = X.shape
     indices = np.arange(N)
-    X_train, X_val, Y_train, Y_val, train_indices, test_indices = train_test_split(X, Y, indices, test_size=0.20)
+    X_train, X_val, Y_train, Y_val, train_indices, test_indices = train_test_split(X, Y, indices, test_size=0.35)
     return X, X_train, X_val, Y, Y_train, Y_val, train_indices, test_indices
 
-def plot_predictions(model, X, X_train, X_val, y, y_train, y_val, codes, title, save_path):
+def get_site_train_test_data(df, pol, train_indices, test_indices, code=None):
+    train_df = df.iloc[train_indices, :]
+    if code:
+        train_df = train_df.loc[train_df["code"] == code]
+    train_X = train_df[["norm_lat", "norm_lon", "t"]].values
+    train_Y = train_df[[f"norm_{pol}"]].values
+    
+    test_df = df.iloc[test_indices, :]
+    if code:
+        test_df = test_df.loc[test_df["code"] == code]
+    test_X = test_df[["norm_lat", "norm_lon", "t"]].values
+    test_Y = test_df[[f"norm_{pol}"]].values
+
+    if code:
+        all_df = df.loc[df["code"] == code]
+    all_X = all_df[["norm_lat", "norm_lon", "t"]].values
+    all_Y = all_df[[f"norm_{pol}"]].values
+    
+    return all_X, all_Y, train_X, train_Y, test_X, test_Y
+
+def plot_predictions(model, df, train_indices, test_indices, codes, title, save_path): #, X, X_train, X_val, y, y_train, y_val, codes, title, save_path):
     '''
     Plots predictions before and after training GPR model for given site code sensor location
     :param site_code: str label of sensor location
@@ -133,44 +160,48 @@ def plot_predictions(model, X, X_train, X_val, y, y_train, y_val, codes, title, 
     
     noise_std = 0.75
 
-    # train_X, train_Y, test_X, test_Y = get_site_train_test_data(code)
     for code in codes:
-        if (X_val.shape[0] > 0): # only plot if we have test data
+        all_X, all_Y, train_X, train_Y, test_X, test_Y = get_site_train_test_data(df, "no2", train_indices, test_indices, code)
+        if (test_X.shape[0] > 0): # only plot if we have test data
             plt.figure(figsize=(12, 4))
             
-            mean_prediction, std_prediction = model.predict(X, return_std=True) # Predict Y values at test locations
-            X_val_t = X_val[:, 2] # only plot time for a given site
-            # print("Validation X:")
-            # print(X_val_t)
+            all_mean_prediction, all_std_prediction = model.predict(all_X, return_std=True) # Predict Y values at test locations
+            all_X_t = all_X[:, 2] # only plot time for a given site
+            all_mean_prediction = all_mean_prediction.flatten()
 
-            train_X_t = X_train[:, 2] # only plot time for a given site
-            # print("Train X:")
-            # print(train_X_t)
+            test_mean_prediction, test_std_prediction = model.predict(test_X, return_std=True)
+            test_X_t = test_X[:, 2]
+            test_mean_prediction = test_mean_prediction.flatten()
 
-            plt.plot(train_X_t, y_train, "x", label="Training points", alpha=0.2, color="green")
+            train_X_t = train_X[:, 2] # only plot time for a given site
 
-            X_val_t, mean_prediction = zip(*sorted(zip(X_val_t, mean_prediction)))
-            (line,) = plt.plot(X_val_t, mean_prediction, lw=1.5, label="Mean of predictive posterior")
+            plt.plot(train_X_t, train_Y, "x", label="Training points", alpha=0.9, color="green", linestyle="None")
+            plt.plot(test_X_t, test_mean_prediction, "o", label="Testing point prediction", alpha=0.9, color="red", linestyle="None")
+            plt.plot(test_X_t, test_Y, "o", label="Testing point actual value", alpha = 0.9, color="orange", linestyle="None")
+
+            all_X_t, all_mean_prediction = zip(*sorted(zip(all_X_t, all_mean_prediction)))
+            (line,) = plt.plot(all_X_t, all_mean_prediction, lw=1.5, label="Mean of predictive posterior")
             col = line.get_color() 
             
-            plt.errorbar(
-                train_X_t,
-                y_train,
-                noise_std,
-                linestyle="None",
-                color="tab:blue",
-                marker=".",
-                markersize=10,
-                label="Observations",
-            )
+            # plt.errorbar(
+            #     train_X_t,
+            #     train_Y,
+            #     noise_std,
+            #     linestyle="None",
+            #     color="tab:blue",
+            #     marker=".",
+            #     markersize=10,
+            #     label="Observations",
+            # )
+
             plt.fill_between(
-                X_val_t,
+                all_X_t,
                 # (mean_prediction - 2 * std_prediction ** 0.5)[:, 0],
                 # (mean_prediction + 2 * std_prediction ** 0.5)[:, 0],
-                (mean_prediction - 1.96 * std_prediction)[:, 0],
-                (mean_prediction + 1.96 * std_prediction)[:, 0],
+                all_mean_prediction - 1.96 * all_std_prediction,#[:, 0],
+                all_mean_prediction + 1.96 * all_std_prediction,#[:, 0],
                 color=col,
-                alpha=0.5,
+                alpha=0.8,
                 label=r"95% confidence interval"
             )
             # Z = model.inducing_variable.Z.numpy()
@@ -179,9 +210,11 @@ def plot_predictions(model, X, X_train, X_val, y, y_train, y_val, codes, title, 
             
             # loss = model.training_loss((X_val, y_val))
             # loss = 0
-            # score = model.score(X_val_t, mean_prediction)
-            score = 0
-            plt.title(f"{title} ({code}); val score (n={X_val.shape[0]})={score}")
+            score = model.score(test_X, test_Y)
+            # score = 0
+            plt.title(f"{title} ({code}); val score (n={test_X.shape[0]})={score}")
+            plt.xlabel(f"Timestep")
+            plt.ylabel(f"Normalized concentration of NO2")
             
             # create folder for model run
             isExist = os.path.exists(save_path)
@@ -197,7 +230,7 @@ if __name__ == "__main__":
     # parameters
     pollutant = "NO2"
     timestep = "D"
-    subset = datetime(2021, 1, 1)
+    subset = datetime(2020, 1, 1)
     season = "summer"
     dayOfWeek = "weekday"
     timeOfDay = None # "day" # timestep must be "H" in order to create time of day buckets
@@ -206,35 +239,45 @@ if __name__ == "__main__":
 
     print("Getting data...")
     # change subset param for all months
-    monthly_NO2_df = load_data(pollutant, data_path="code/data-collection/LAQN_API_data/", timestep=timestep, subset=subset, season=season, dayOfWeek=dayOfWeek, timeOfDay=timeOfDay)
+    df = load_data(pollutant, data_path="../../data/", timestep=timestep, subset=subset, season=season, dayOfWeek=dayOfWeek, timeOfDay=timeOfDay)
 
     print("Preparing data...")
-    monthly_NO2_df, scalers = transform_data(monthly_NO2_df, pollutant)
-    X, X_train, X_val, Y, Y_train, Y_val, train_indices, test_indices = get_X_Y_split(monthly_NO2_df, pollutant)
-    site_codes = monthly_NO2_df["SiteCode"].unique()
+    df, scalers = transform_data(df, pollutant.lower(), timestep)
+    X, X_train, X_val, Y, Y_train, Y_val, train_indices, test_indices = get_X_Y_split(df, pollutant.lower())
+    site_codes = df["code"].unique()
+    print(df.head())
 
     print("Creating model...")
-    kernel = kernels.ExpSineSquared(periodicity=12)*kernels.RBF([0, 0, 1.0]) + kernels.RBF([1.0, 1.0, 0])
+    PERIODICITY = 30
+    kernel = kernels.ExpSineSquared(periodicity=PERIODICITY)*kernels.RBF([0, 0, 1.0]) + kernels.RBF([1.0, 1.0, 0])
     model = GaussianProcessRegressor(kernel=kernel, alpha=1e-10)
 
-    # plot_predictions(model, X, X_train, X_val, Y, Y_train, Y_val, site_codes, title="Predictions before training", save_path="code/gp-models/GPR_figures/"+ figure_title + "/preTraining/")
+    # plot_predictions(model, X, X_train, X_val, Y, Y_train, Y_val, site_codes, title="Predictions before training", save_path="code/gp-models/GPR_figures/" + figure_title + "/preTraining/")
+    # plot_predictions(model, df, train_indices, test_indices, site_codes, title="Predictions before training", save_path="GPR_figures/" + figure_title + "/preTraining/")
 
     print("Training model...")
-    start = time.process_time()
-    print(f"Started at {start}")
+    start_time = time.process_time()
+    print(f"Started at {start_time}")
     model.fit(X_train, Y_train)
     model.fit(X_train, Y_train)
-    elapsed_time = time.process_time() - start
-    print(f"Finished at {elapsed_time}")
+    elapsed_time = time.process_time()
+    print(f"Finished after {elapsed_time-start_time}")
 
     # plot results
-    plot_predictions(model, X, X_train, X_val, Y, Y_train, Y_val, site_codes, title="Predictions after training", save_path="code/gp-models/GPR_figures/" + figure_title + "/postTraining/")
+    # plot_predictions(model, X, X_train, X_val, Y, Y_train, Y_val, site_codes, title="Predictions after training", save_path="code/gp-models/GPR_figures/" + figure_title + "/postTraining/")
+    plot_predictions(model, df, train_indices, test_indices, site_codes, title="Predictions after training", save_path="GPR_figures/" + figure_title + "/postTraining/")
 
     print("Scoring model...")
-    model.score(X_val, Y_val)
-    #final_train_X, final_train_Y, final_test_X, final_test_Y = get_site_train_test_data()
+    print(model.score(X_val, Y_val))
+    # final_train_X, final_train_Y, final_test_X, final_test_Y = get_site_train_test_data()
 
     print("Saving model...")
-    filename = "code/gp-models/sklearn_GPR_no2_monthly.sav"
-    pickle.dump(model, open(filename, "wb"))
-    print("Successfully trained and saved model.")
+    random_id = random.randint(0, 10000)
+    filename = f"{random_id}_{figure_title}"
+    df_filename = f"{filename}_df.sav"
+    model_filename = f"{filename}_model.sav"
+    pickle.dump(df, open(df_filename, "wb"))
+    pickle.dump(model, open(model_filename, "wb"))
+    print("Successfully trained and saved model at:")
+    print(df_filename)
+    print(model_filename)
